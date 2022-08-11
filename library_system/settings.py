80da9272e2f407f71818dca41c99f0c6cec2fcf2
DanielMaldonadoMora79 <67204992+DanielMaldonadoMora@users.noreply.github.com>
2022-08-10 22:33:33 -0500

    Update settings.py

--- library_system/settings.py
+++ library_system/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-^=b(**$79tz5h0289mcjvg4k(=k60pn6yuu^+qe&orkab12vyc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django_filters',
     'drf_yasg',
     'library_sys',
+    'borrow_system',
     'books',
     'users'
 ]
@@ -109,13 +110,12 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
    
     'DEFAULT_AUTHENTICATION_CLASSES': (
-       
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
     
 }
 SIMPLE_JWT = {
-    'ACCES_TOKEN_LIFETIME': datetime.timedelta(hours=3),
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(hours=3),
     'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=7)
 }
 
